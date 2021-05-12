0e3851a47e8050b04664cae4065358b0c319e525
Sandeep-Kumawat <sandeepkumawatsa77@gmail.com>
2021-05-12 20:17:19 +0530

    Product Catalog using FileStreams

---
+++ Revision Day2/ProductCatalog/ProductData/Menu/CategoryMenu.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductData.Menu
+{
+    public class CategoryMenu
+    {
+        public static void CategoryOperationMenu()
+        {
+            Console.WriteLine("Please Select Category Operation");
+            Console.WriteLine("a. Enter a Category");
+            Console.WriteLine("b. List all Categories");
+            Console.WriteLine("c. Delete a Category");
+            Console.WriteLine("d. Search a Category");
+            Console.WriteLine("e. Main Menu");
+            char ch1 = Convert.ToChar(Console.ReadLine());
+
+            switch (ch1)
+            {
+                case 'a':
+                    Console.WriteLine("Enter Category Name");
+                    var categoryName = Console.ReadLine();
+                    while (string.IsNullOrWhiteSpace(categoryName) || int.TryParse(categoryName, out _))
+                    {
+                        Console.WriteLine("Please Enter Only Char and It can not be Empty");
+                        categoryName = Console.ReadLine();
+                        //break;
+                    }
+                    Console.WriteLine("Enter Short Code");
+                    var shortCode = Console.ReadLine();
+
+                    Console.WriteLine("Enter Description");
+                    var desc = Console.ReadLine();
+                    while (string.IsNullOrWhiteSpace(desc) || int.TryParse(desc, out _))
+                    {
+                        Console.WriteLine("Please Enter Only Char and It can not be Empty");
+                        desc = Console.ReadLine();
+                    }
+                    
+                    break;
+                case 'b':
+                    
+                    Console.ReadKey();
+                    break;
+                case 'c':
+                    
+                    break;
+                case 'd':
+                  
+                    break;
+                case 'e':
+
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    CategoryOperationMenu();
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Revision Day2/ProductCatalog/ProductData/Operation/ProductOperation.cs
@@ -0,0 +1,44 @@
+﻿using ProductData.Entities;
+using ProductData.FileManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductData.Operation
+{
+    public class ProductOperation
+    {
+        
+        static List<Product> data = ProductFileManager.ReadDataFromFile();
+        static int lastId = data[data.Count - 1].Product_ID;
+        public static void GetAllProduct()
+        {
+           
+            data.ForEach((i) =>
+            {
+                Console.WriteLine($"{i.Product_ID}, {i.product_Name}, {i.Manufacturer}," +
+                    $" {i.ProductShortCode}, {i.ProductCategory}, {i.ProductDescription}, {i.Selling_Price}");
+            });
+            Console.ReadKey();
+           
+        }
+        public static void AddProduct(string ProductName, string shortCode, string desc, int price, string manufactureName,string category)
+        {
+
+            var addproductdata = $"{++lastId},{ProductName},{manufactureName},{shortCode},{category},{desc},{price}";
+            ProductFileManager.AddProductInFile(addproductdata);
+            data = ProductFileManager.ReadDataFromFile();
+            //data.Add(new Product
+            //{
+            //   product_Name=ProductName,
+            //    Manufacturer = manufactureName,
+            //    ProductShortCode = shortCode,
+            //    ProductCategory = category,
+            //    ProductDescription = desc,
+            //    Selling_Price = price
+            //});
+            Console.ReadKey();
+
+        }
+    }
+}
